530ef2a2493dc67f2823f5ca47b4d1425eb9ef75
justinecanlas <75960447+justinecanlas@users.noreply.github.com>
2021-11-21 20:47:57 -0600

    Create buffer.h

---
+++ buffer.h
@@ -0,0 +1,70 @@
+//------------------------------------------------------------------
+// buffer.h
+// Buffer class 
+// Author: Justine Canlas
+//------------------------------------------------------------------
+// This header file includes the buffer class 
+// It includes the data members needed for the 
+//------------------------------------------------------------------
+
+#ifndef BUFFER.H
+#define BUFFER.H
+/**
+ * @file buffer.h
+ * @brief This is the header file for the buffer class
+ *
+ * @author Justine Canlas
+ * 
+ */
+
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class BlockBuffer
+{
+  private:
+  
+  public:
+  
+  /**
+   * Default Constructor
+   */
+  BlockBuffer();
+  
+  /**
+  *
+  * @param
+  * @return
+  * @pre
+  * @post
+  */
+  int NumberofRecords
+  
+  /**
+  *
+  * @param
+  * @return
+  * @pre
+  * @post
+  */
+  const int getNumRecs();
+  
+  /**
+  *
+  * @param
+  * @return
+  * @pre
+  * @post
+  */
+  int Read(
+  
+  /**
+  *
+  * @param
+  * @return
+  * @pre
+  * @post
+  */
+  int Unpack
