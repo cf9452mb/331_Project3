4de43629d520dfaa00438840d1cc29b34bb17834
cf9452mb <79483028+cf9452mb@users.noreply.github.com>
2021-11-22 12:03:41 -0600

    Header file for Location Objects

--- Location.h
+++ Location.h
@@ -50,101 +50,101 @@ class Location
         /// @return The current value of zip code
         string getKey() const;
         
-		    /// @brief Access zip code
+		/// @brief Access zip code
         /// @return The current value of zip code
         string getZipCode() const;
 		
-		    /// @brief Set zip code
+		/// @brief Set zip code
         /// @param New value of zipcode
         /// @post Sets val to zipCode
         void setZipCode(const string &val);
       
-		    /// @brief Access place name
+		/// @brief Access place name
         /// @return The current value of place name
         string getPlace() const;       
 
-		    /// @brief Set place name
+		/// @brief Set place name
         /// @param New value of place
         /// @post Sets val to place
         void setPlace(const string &val);
 	    
-	      /// @brief Access state
+	    /// @brief Access state
         /// @return The current value of state
         string getState() const;
 		      
-	      /// @brief Set state
+	    /// @brief Set state
         /// @param New value of state
         /// @post Sets val to state
         void setState(const string &val);
 
-		    /// @brief Access county
+		/// @brief Access county
         /// @return The current value of county
         string getCounty() const;    
 
-	      /// @brief Set county
+	    /// @brief Set county
         /// @param New value of county
         /// @post Sets val to county
         void setCounty(const string &val);
 		   
-	      /// @brief Access latitude
+	    /// @brief Access latitude
         /// @return The current Latitude value
         float getLat() const;
       
-	      /// @brief Set latitude
+	    /// @brief Set latitude
         /// @param New value of latitude
         /// @post Sets val to latitude
         void setLat(const float &val);
        
-	      /// @brief Access longitude
+	    /// @brief Access longitude
         /// @return The current Longitude value
         float getLong() const;
       
-	      /// @brief Set longitude
+	    /// @brief Set longitude
         /// @param New value of longtitude
         /// @post Sets val to longtitude
         void setLong(const float &val);
        
-		    /// @brief This method unpacks data from a buffer and pass it into the Location object
-		    /// @param DelimBuffer object buffer
-		    /// @pre buffer must be declared and readed the data file before calling this method
-		    /// @post Pass the next fields data to the Location object
-		    /// @return Returns true if unpacked successfully
+		/// @brief This method unpacks data from a buffer and pass it into the Location object
+		/// @param DelimBuffer object buffer
+		/// @pre buffer must be declared and readed the data file before calling this method
+		/// @post Pass the next fields data to the Location object
+		/// @return Returns true if unpacked successfully
         bool unpack(DelimBuffer &buffer);
         
-		    /// @brief Overloaded assignment operator for a location object
-		    /// @param Location class object
-		    /// @post The location object in the left now has the same data as the Location object in the right
+		/// @brief Overloaded assignment operator for a location object
+		/// @param Location class object
+		/// @post The location object in the left now has the same data as the Location object in the right
         void operator= (const Location &loc);
 
-		    /// @brief Less than comparison operator overloaded
-		    /// @param Location class object
-		    /// @return True if the left state come later than the right state in alphabet order, false otherwise.	
-		    bool operator< (const Location &loc) const;
+		/// @brief Less than comparison operator overloaded
+		/// @param Location class object
+		/// @return True if the left state come later than the right state in alphabet order, false otherwise.	
+		bool operator< (const Location &loc) const;
 
-		    /// @brief Greater than comparison operator overloaded
-		    /// @param Location class object
-		    /// @return true if the left state come sooner than the right state in alphabet order, false otherwise.		
-		    bool operator> (const Location &loc) const;
+		/// @brief Greater than comparison operator overloaded
+		/// @param Location class object
+		/// @return true if the left state come sooner than the right state in alphabet order, false otherwise.		
+		bool operator> (const Location &loc) const;
         
         /// @brief outstream operator overloaded
-		    /// @param ostream 
-		    /// @param Location class object
-		    /// @return returns the overloaded operator right hand operand
+		/// @param ostream 
+		/// @param Location class object
+		/// @return returns the overloaded operator right hand operand
         friend ostream& operator<< (ostream& out, const Location &loc);
         
         /// @brief This method get the weight of the Location object
-		    /// @return The weight of the Location object
+		/// @return The weight of the Location object
         int getWeight() const;
 
     private:
-    	  /// Member variable for zip code
+    	/// Member variable for zip code
         string zipcode; 
         /// Member variable for place name
         string place;
         /// Member variable for state
         string state;
-		    /// Member variable for county
-		    string county;
+		/// Member variable for county
+		string county;
         /// Member variable for latitiude
         float latitude; 
         /// Member variable for longitude
