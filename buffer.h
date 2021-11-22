f93163321b4115a9493bec167f03c266dcaf4c5f
cf9452mb <79483028+cf9452mb@users.noreply.github.com>
2021-11-21 20:55:38 -0600

    Added data members

--- buffer.h
+++ buffer.h
@@ -26,6 +26,29 @@ class BlockBuffer
 {
   private:
   
+  /// Delimiter
+	char delim = ',';
+	/// The buffer
+	string buffer; 
+	/// Size of the buffer string
+	unsigned long bufferSize = 0; 
+	/// Largest buffer size, default = size of string
+	unsigned int maxByte = UINT_MAX2;
+	/// Index of next char
+	int nextCharIndex = 0;
+	/// Number of records
+	int numRecs = 0;
+	/// Block number
+	int blockNumber = 0;
+	/// Succeeded block number
+	int sBlockNumber = 0;
+	/// Succeeded block number
+	int pBlockNumber = 0;
+	/// @brief Set the current block's size to a certain value
+	void setNumRecs(const int &val);
+	/// @brief Set the current block's number to a certain value
+	void setBlockNumber(const int &val);
+  
   public:
   
   /**
